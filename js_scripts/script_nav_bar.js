01c404fb00494220778b758f8778018afc2246d4
VitaliyMorix <vitalik2001vaskin@gmail.com>
2021-12-07 20:18:26 +0300

    Add animation for the test text

--- js_scripts/script_nav_bar.js
+++ js_scripts/script_nav_bar.js
@@ -1,18 +1,18 @@
-const navBar = document.querySelectorAll('.fix_nav_bar');
+const boxes2 = document.querySelectorAll('#axis');
 
-window.addEventListener('scroll', checkBoxes);
-checkNavBar(); // shows initial box(es) 
+  window.addEventListener('scroll', checkAxis);
+  checkAxis(); // shows initial box(es) 
 
-function checkNavBar() {
-  const triggerBottom = (window.innerHeight / 5 * 40);
-  
-  navBar.forEach(fix_nav_bar => {
-    const boxTop = navBar.getBoundingClientRect().top;
+  function checkAxis() {
+    const triggerBottom2 = (window.innerHeight/5*4);
     
-    if(boxTop < triggerBottom) {
-      box.classList.add('addShow');
-    } else {
-      box.classList.remove('addShow');
-    }
-  })
-}
+    boxes2.forEach(axis => {
+      const boxTop2 = axis.getBoundingClientRect().top;
+      
+      if(boxTop2 < triggerBottom2) {
+        axis.classList.add('active');
+      } else {
+        axis.classList.remove('active');
+      }
+    })
+  }
