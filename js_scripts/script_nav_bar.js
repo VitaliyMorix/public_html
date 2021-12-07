0311b9cc70fe08f3bcbc93a354774f23059f0db3
VitaliyMorix <vitalik2001vaskin@gmail.com>
2021-12-07 23:57:42 +0300

    Create new animation for sidebar

--- js_scripts/script_nav_bar.js
+++ js_scripts/script_nav_bar.js
@@ -1,4 +1,4 @@
-const boxes2 = document.querySelectorAll('#axis');
+const boxes2 = document.querySelectorAll('.sideBarAmin');
 
   window.addEventListener('scroll', checkAxis);
   checkAxis(); // shows initial box(es) 
@@ -7,12 +7,10 @@ const boxes2 = document.querySelectorAll('#axis');
     const triggerBottom2 = (window.innerHeight/5*4);
     
     boxes2.forEach(axis => {
-      const boxTop2 = axis.getBoundingClientRect().top;
+      const boxTop2 = sideBarAmin.getBoundingClientRect().top;
       
       if(boxTop2 < triggerBottom2) {
-        axis.classList.add('active');
-      } else {
-        axis.classList.remove('active');
+        sideBarAmin.classList.add('show');
       }
     })
   }
