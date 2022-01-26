468af286abf4bbbc1e745738b06ff9ea5483ef69
VitaliyMorix <vitalik2001vaskin@gmail.com>
2022-01-26 14:19:56 +0300

    Test php-header

---
+++ file_php/header.php
@@ -0,0 +1,13 @@
+<header>
+    <nav>
+        <div class="header-setup-style">
+            <div class="setup-menu-header">
+                <a href="./file_php/post.html">Примеры<br>на PHP</a>
+                <a href="./html_file/js_test.html">Примеры<br>на JS</a>
+                <div class="name-site">the most <br> boring site</div>
+                <a href="index.html">О сайте</a>
+                <a href="./html_file/site.html">Основная<br>информация</a>
+            </div>
+        </div>
+    </nav>
+</header>
